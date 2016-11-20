6be6fc39fa43251cae3eaa641b086f4f84139694
getweapp <yw@getweapp.com>
2016-11-20 19:01:23 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,30 @@
+# 微信小程序-QQ
+
+### 说明：
+
+简单的QQ客户端，实现了消息列表，搜索，联系人，动态，音乐播放等功能。
+
+注意：音乐信息使用的第三方接口，由于是免费的，限制每秒请求2次，所以可能会出现音乐加载失败问题。
+
+### 数据接口:
+
+- https://api.getweapp.com/thirdparty/qq/music
+
+### 目录结构：
+
+- images — 存放项目图片
+- pages — 存放项目页面相关文件，一共包括contact,dynamic,music,index,logs,message,chat,search等几个页面
+- utils — 存放日期格式化文件
+
+### 开发环境：
+
+微信web开发者工具 v0.10.102800
+
+### 项目截图：
+
+https://www.getweapp.com/project?projectId=583181f2bb2538f8186c705b
+
+### 感谢:
+
+本项目原始版本由xiehui999提供：https://github.com/xiehui999/SmallAppForQQ
+
---
+++ pages/dynamic/music/music.js
@@ -0,0 +1,97 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    select1:true,
+    select2:false,
+    select3:false,
+    isHiddenToast:true,
+    songlist:[],
+  },
+  isShowToast:function(){
+    this.setData({
+      isHiddenToast:false
+    })
+  },
+  toastChange:function(){
+    this.setData({
+      isHiddenToast:true
+    })
+  },
+  tab1:function(){
+ this.setData({
+    select1:true,
+    select2:false,
+    select3:false,
+ })
+  },
+  tab2:function(){
+ this.setData({
+    select1:false,
+    select2:true,
+    select3:false,
+ })
+  },
+  tab3:function(){
+ this.setData({
+    select1:false,
+    select2:false,
+    select3:true,
+ })
+  },
+    play: function (event) {
+    var that = this
+    console.log(event);
+    var res=that.data.songlist[event.currentTarget.dataset.num]
+    wx.playBackgroundAudio({
+      dataUrl: res.url,
+      name: res.songname,
+      singer:res.singername,
+      coverImgUrl: res.albumpic_small,
+      complete: function (res) {
+      }
+    })
+  },
+  randomStart:function(event){
+    var that = this
+    var random=Math.floor(Math.random()*50);
+    var res=that.data.songlist[random]
+    wx.playBackgroundAudio({
+      dataUrl: res.url,
+      name: res.songname,
+      singer:res.singername,
+      coverImgUrl: res.albumpic_small,
+      complete: function (res) {
+      }
+    })
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+        var that = this
+    wx.request({
+      url: 'https://api.getweapp.com/thirdparty/qq/music',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+          console.log(res);
+          console.log(res.data.showapi_res_body.pagebean.songlist);
+         that.setData({
+           songlist: res.data.showapi_res_body.pagebean.songlist
+         })
+      }
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+    //NavigationBarTitle如果此处和json文件都设置，最后展示此处的标题栏
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
